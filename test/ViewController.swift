fd4fb4808be696ce873804420386a339bbfcd09a
Mozy Okubo <mozy.okubo@gmail.com>
2019-04-05 00:42:54 +0900

    add camera roll picker

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
+    @IBAction func ButtonTapped(_ sender: Any) {
+        let c = UIImagePickerController()
+        c.mediaTypes = [kUTTypeMovie as String]
+        present(c, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
