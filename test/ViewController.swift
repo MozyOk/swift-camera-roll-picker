acfc203d1e0293c4722acaf35b319fa0f727bc8c
Mozy Okubo <mozy.okubo@gmail.com>
2019-04-05 00:31:16 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by okubo_m on 2019/04/05.
+//  Copyright © 2019 mozy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
